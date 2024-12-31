50daaab8c9efc969c54421b5805415652e63d8c4
g-gundam <g-gundam@users.noreply.github.com>
2024-12-31 01:21:38 -0800

    Add rocket.jl with ChartSubject definition

---
+++ src/rocket.jl
@@ -0,0 +1,41 @@
+using Rocket
+
+# consumes: Candle
+# emits:    Tuple{Symbol, Symbol, Candle}
+#           Tuple{Symbol, Symbol, Float64}
+#
+# Symbol 1 = chart name
+# Symbol 2 = :update or :add
+# The rest is new data for the client-side charts.
+@kwdef mutable struct ChartSubject <: Rocket.AbstractSubject{Any}
+    charts::Dict{Symbol,Chart}
+    subscribers::Vector = []
+end
+
+function Rocket.on_subscribe!(subject::ChartSubject, actor)
+    push!(subject.subscribers, actor)
+    return voidTeardown
+end
+
+function Rocket.on_next!(subject::ChartSubject, c::Candle)
+    for (k, v) in subject.charts
+        complete_candle = TechnicalIndicatorCharts.update!(v, c)
+        if !isnothing(complete_candle)
+            # This is the :add block.
+            for s in subject.subscribers
+                next!(s, (k, :add, complete_candle))
+                # TODO: Also send everything that is not OHLCV.
+                #next!(s, (k, :add, all-non-ohlcv-fields ))
+            end
+        else
+            # This is the :update block.
+            # Only candles can do updates in this system.
+            # Indicators only change on candle close.
+            for s in subject.subscribers
+                current_candle = subject.charts[k].candle
+                next!(s, (k, :update, current_candle))
+            end
+        end
+        yield()
+    end
+end
