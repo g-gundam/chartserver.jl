e8fa57c32a2d7c3d04f996f1db502ebc954ddb2f
g-gundam <g-gundam@users.noreply.github.com>
2025-01-01 17:00:44 -0800

    Put all the series in an object so I can access them by name

--- www/js/demo.js
+++ www/js/demo.js
@@ -5,20 +5,29 @@ document.addEventListener('DOMContentLoaded', () => {
       textColor: 'black',
       background: { type: 'solid', color: 'white' },
     },
+    rightPriceScale: {
+      mode: 1 // https://tradingview.github.io/lightweight-charts/docs/api/enums/PriceScaleMode
+    },
     autoSize: true,
     width:  640, // fallback if autoSize fails
     height: 640
   };
   const chart = LightweightCharts.createChart(container, chartOptions)
+  chart.timeScale().fitContent()
 
   // TODO: In the next iteration, all this config info needs to come from the server.
   // I want chart state on the server to be reflected on the client in realtime.
-  const candlestickSeries = chart.addCandlestickSeries({
+  const ohlc = chart.addCandlestickSeries({
     upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
     wickUpColor: '#26a69a', wickDownColor: '#ef5350',
   });
   const sma50 = chart.addLineSeries({ color: "#E072A4", width: 2 })
   const sma200 = chart.addLineSeries({ color: "#3D3B8E", width: 5 })
+  const series = {
+    ohlc,
+    sma50,
+    sma200
+  }
 
   // WebSockets
   const wsProtocol = location.protocol == 'http:' ? 'ws' : 'wss'
@@ -38,9 +47,17 @@ document.addEventListener('DOMContentLoaded', () => {
       switch (msg.type) {
       case "update":
         console.log("update", msg)
+        s = series[msg.series]
+        data = msg.data
+        s.update(data)
         break
       case "add":
-        console.log("add", msg)
+        // The server-side makes an add vs update distinction,
+        // but I guess the client doesn't have to.
+        console.log("add", msg) 
+        s = series[msg.series]
+        data = msg.data
+        s.update(data)
         break
       default:
         console.log("default", msg)
