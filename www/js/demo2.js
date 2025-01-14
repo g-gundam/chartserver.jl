46e572e499bd374cebe4651aad1a3592569f3055
g-gundam <g-gundam@users.noreply.github.com>
2025-01-13 18:05:43 -0800

    Add mostly empty demo2.js

---
+++ www/js/demo2.js
@@ -0,0 +1,7 @@
+// Goals
+// - dynamic chart construction based on server-side chart config
+// - realtime data consumption
+
+document.addEventListener('DOMContentLoaded', async () => {
+  console.info("Now what?")
+})
