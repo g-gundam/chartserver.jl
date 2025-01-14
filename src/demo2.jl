ae8aa16af0c157c44681a082731cdf5bec780d1d
g-gundam <g-gundam@users.noreply.github.com>
2025-01-14 03:50:15 -0800

    Dynamically create charts and series

--- src/demo2.jl
+++ src/demo2.jl
@@ -97,7 +97,7 @@ function config(chart::Chart)
         ),
         :autoSize => true,
         :width => 640,
-        :height => 620,
+        :height => 220,
         :_series => Dict(series)
     )
 end
--- www/js/demo2.js
+++ www/js/demo2.js
@@ -2,6 +2,42 @@
 // - dynamic chart construction based on server-side chart config
 // - realtime data consumption
 
+// INFO: Not implementing it this time.
+function createLayout() {
+}
+
+function createChart(config) {
+  const series = {}
+  const el = document.createElement('div')
+  el.classList.add("chart")
+  const chart = LightweightCharts.createChart(el, config)
+  Object.keys(config._series).forEach((k) => {
+    const seriesConfig = config._series[k]
+    switch (seriesConfig._type) {
+    case "ohlc":
+      series[k] = chart.addCandlestickSeries(seriesConfig)
+      break;
+    case "line":
+      series[k] = chart.addLineSeries(seriesConfig)
+      break;
+    }
+  })
+  return { el, chart, series }
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
   console.info("Now what?")
+
+  // fetch charts
+  const res = await fetch("/demo2/charts")
+  const chartConfigs = await res.json()
+
+  const charts = chartConfigs.map(createChart)
+
+  // append to #workspace
+  const workspace = document.getElementById("workspace")
+  charts.forEach((c) => workspace.append(c.el))
+
+  window.chartConfigs = chartConfigs
+  window.charts = charts
 })
